c6d79323f9ea770c64ba5be275c902b5adafec77
matthijsnij <matthijsnijeboer@hotmail.nl>
2025-12-18 09:24:49 +0100

    Update supabaseClient.js

--- code/supabaseClient.js
+++ code/supabaseClient.js
@@ -2,7 +2,7 @@
 const SUPABASE_URL = "https://tdnjzgzyliugcxtkbhrk.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRkbmp6Z3p5bGl1dGtiaHJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5NTM1MjksImV4cCI6MjA4MTUyOTUyOX0.aU5X55DqWb6-SWrRmCCuNO4hnfJSyCbY-FnXSyfBARk";
 
-window.supabase = Supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 window.supabaseClient = window.supabase;
 
 
